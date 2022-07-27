83df09121023b90687b7372329d6833035643961
SAMRIT PRAMANIK <samrit.2442@gmail.com>
2022-07-27 07:53:08 +0530

    notification app

--- notification/app.R
+++ notification/app.R
@@ -1,9 +1,19 @@
 library(shiny)
 
-ui <- 
+ui <- fluidPage(
+  actionButton("goodnight", "Good Night")
+)
 
-
-
-
+server <- function(input, output, session) {
+  observeEvent(input$goodnight, {
+    showNotification("So long")
+    Sys.sleep(1)
+    showNotification("Farewell", type = "message")
+    Sys.sleep(1)
+    showNotification("Shubho Ratri", type = "warning")
+    Sys.sleep(1)
+    showNotification("Abar asibo phire", type = "error")
+  })
+}
 
 shinyApp(ui = ui, server = server)
