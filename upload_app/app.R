7c07025b3108bfab9c845546924657c6f077f3dc
SAMRIT PRAMANIK <samrit.2442@gmail.com>
2022-08-06 05:41:39 +0530

    upload

--- upload_app/app.R
+++ upload_app/app.R
@@ -2,11 +2,12 @@ library(shiny)
 
 ui <- fluidPage(
   fileInput("upload", "Upload a file", accept = c(".csv, .xlsx, .txt"),
-            placeholder = "Nibiru has not uploaded a file yet", width = "400px")
+            placeholder = "Nibiru has not uploaded a file yet", width = "400px"),
+  tableOutput("files")
 )
 
-
-
-
+server <- function(input, output, session) {
+  output$files <- renderTable(input$upload)
+}
 
 shinyApp(ui = ui, server = server)
