ccebc81c57328d84037222f0d3ddbdb907303b8d
SAMRIT PRAMANIK <samrit.2442@gmail.com>
2022-08-11 16:10:24 +0530

    uploading file uploaded

---
+++ upload_app/uploading/app.R
@@ -0,0 +1,26 @@
+library(shiny)
+
+ui <- fluidPage(
+  fileInput("file", NULL, accept = c(".csv", ".tsv")),
+  numericInput("n", "Rows", value = 5, min = 1, step = 1),
+  tableOutput("head")
+)
+
+server <- function(input, output, session) {
+  data <- reactive({
+    req(input$file)
+    
+    ext <- tools::file_ext(input$file$name)
+    switch(ext,
+           csv = vroom::vroom(input$file$datapath, delim = ","),
+           tsv = vroom::vroom(input$file$datapath, delim = "\t"),
+           validate("Invalid file; Please upload a .csv or .tsv file")
+           )
+  })
+  
+  output$head <-renderTable({
+    head(data(), input$n)
+  })
+}
+
+shinyApp(ui = ui, server = server)
